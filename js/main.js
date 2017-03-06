f2f5bed08576895bda5b46014aa88d803822a81d
Pachj <Pachj@users.noreply.github.com>
2017-03-06 13:57:19 +0100

    can fill the input into the buttons and also can reset it

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 /**
  * Created by Henry on 06.03.17.
  */
+!function () {
+    $(document).ready(function () {
+        $("#first-row, #second-row, #third-row").children().click(function () {
+            fillInInput(this.id);
+        });
+    });
+
+    function fillInInput(id) {
+        id = "#" + id;
+
+        if ($(id).html().length === 0) {
+            $(id).html("<i class='fa fa-times'></i>");
+        }
+    }
+
+    function resetGame() {
+        const fields = ["#field-one", "#field-two", "#field-three", "#field-four", "#field-five",
+            "#field-six", "#field-seven", "#field-eight", "#field-nine"];
+
+        for (let i = 0; i < fields.length; i++) {
+            let field = fields[i];
+            $(field).html("");
+        }
+    }
+}();
