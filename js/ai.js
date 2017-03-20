0232a9b9249345575f76a8d924e7c6bf25fcb69a
Pachj <Pachj@users.noreply.github.com>
2017-03-20 11:38:33 +0100

    restart of the project

--- js/ai.js
+++ js/ai.js
@@ -1,10 +1,3 @@
 /**
  * Created by Henry on 13.03.17.
  */
-
-class Ai extends Player{
-  constructor(symbol) {
-    super(symbol);
-    this.moves = 0;
-  }
-}
--- js/controller.js
+++ js/controller.js
@@ -1,23 +1,3 @@
 /**
  * Created by Henry on 13.03.17.
  */
-let myApp;
-
-$(document).ready(function() {
-  $('#choose-symbol').css('display', 'block');
-
-  $('#cross, #circle').click(function() {
-    let symbols = [];
-    symbols[0] = $(this).attr('value');
-    symbols[1] = (symbols[0] === 'x' ? 'o' : 'x');
-    myApp = new App(symbols);
-
-    Ui.showOrHideElement($('#choose-symbol'));
-  });
-
-  $('.game-field').click(function() {
-    const id = this.id;
-    const value = parseInt($(this).attr('value'));
-    myApp.actualGame.actionHandler(value, id);
-  });
-});
