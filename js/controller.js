2a44cd5ec869b30acff0abfe388ce39489059380
Pachj <Pachj@users.noreply.github.com>
2017-03-20 13:38:37 +0100

    show the symbol select at the beginning

--- js/controller.js
+++ js/controller.js
@@ -1,3 +1,9 @@
 /**
  * Created by Henry on 13.03.17.
  */
+
+ const myApp = new App();
+
+$(document).ready(() => {
+  Ui.showOrHideElement('choose-symbol');
+});
--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,10 @@
 /**
  * Created by Henry on 13.03.17.
  */
+
+class Ui {
+  static showOrHideElement(elementId) {
+    const newProperty = $('#' + elementId).css('display') === 'none' ? 'block' : 'none';
+    $('#' + elementId).css('display', newProperty);
+  }
+}
