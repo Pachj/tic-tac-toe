b4342377155ee8b678e084174272839e3b018717
Pachj <Pachj@users.noreply.github.com>
2017-03-25 11:12:08 +0100

    working on ai

--- js/ai.js
+++ js/ai.js
@@ -1,3 +1,88 @@
 /**
  * Created by Henry on 07.03.17.
  */
+class gameState {
+  constructor(board, actualPlayer) {
+    this.board = board;
+    this.actualPlayer = actualPlayer;
+    this.currentDepth = 0;
+  }
+
+  clone() {
+    let newGameState = new gameState(this.board.slice(0), this.actualPlayer);
+    newGameState.currentDepth = this.currentDepth + 1;
+    return newGameState;
+  }
+
+  getEmptyFields() {
+    let emptyFields = [];
+    this.board.forEach((field, index) => {
+      if (field === 'e') {
+        emptyFields.push(index);
+      }
+    });
+    return emptyFields;
+  }
+
+  applyTo(field) {
+    this.board[field] = this.actualPlayer;
+  }
+
+  switchPlayer() {
+    this.actualPlayer = this.actualPlayer === 'x' ? 'o' : 'x';
+  }
+
+  /** checks if the player who had made the new move has won
+   * @return {Boolean} whether the player has won
+   */
+  checkActualPlayerHasWon() {
+    const winningConditions = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+
+    let checkActualCondition = (condition) => {
+      if (this.board[condition[0]] === this.actualPlayer) {
+        if (this.board[condition[1]] === this.actualPlayer) {
+          if (this.board[condition[2]] === this.actualPlayer) {
+            return true;
+          }
+        }
+        return false;
+      }
+    };
+
+    for (let i = 0; i < winningConditions.length; i++) {
+      if (checkActualCondition(winningConditions[i])) {
+        return true;
+      }
+    }
+    return false;
+  };
+}
+
+function getMove(gameState) {
+  console.log('#' + gameState.currentDepth + ' actualState: ' +
+      gameState.board.toString() + ' actualPlayer: ' +
+      gameState.actualPlayer.toString());
+
+  if (gameState.checkActualPlayerHasWon()) {
+    console.log('#' + gameState.currentDepth + ' player has won: ' +
+        gameState.actualPlayer);
+
+    return gameState.actualPlayer === 'x' ? 10 : -10;
+  }
+  const emptyFields = gameState.getEmptyFields();
+
+  if (emptyFields.length === 0) {
+    console.log('#' + gameState.currentDepth + ' patt!');
+    return 0;
+  }
+
+  emptyFields.forEach((field) => {
+    let newGameState = gameState.clone();
+    newGameState.applyTo(field);
+    newGameState.switchPlayer();
+
+    getMove(newGameState);
+  });
+}
