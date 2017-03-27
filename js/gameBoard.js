b2558055ec38c445f0c24272b4152cafb3851c34
Pachj <Pachj@users.noreply.github.com>
2017-03-27 14:56:38 +0200

    can display the moves and add them to the board

--- js/gameBoard.js
+++ js/gameBoard.js
@@ -7,18 +7,25 @@
  */
 
 (function() {
-  const singlePlayer = true;
-  // which player can make the first move in the round
-  let player1HasFirstMove = false;
-
   // the player
   const player1 = {
-    symbol: undefined
+    symbol: undefined,
+    symbolForDisplay: undefined,
   };
   // the second player or the AI
   const player2 = {
-    symbol: undefined
+    symbol: undefined,
+    symbolForDisplay: undefined,
   };
+
+  const isSinglePlayer = true;
+
+  // which player can make the first move in the round
+  let player1HasFirstMove = false;
+
+  let actualPlayer = player1HasFirstMove === true ? player1 : player2;
+
+  let board = ['e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'];
 
   /** choose the selected symbol for player1 and the opposite for player2
    * @param {String} selectedSymbol - the id of the selected button*/
@@ -27,11 +34,15 @@
     const circle = 'fa fa-circle-o';
 
     if (selectedSymbol === 'cross') {
-      player1.symbol = cross;
-      player2.symbol = circle;
+      player1.symbol = 'x';
+      player1.symbolForDisplay = cross;
+      player2.symbol = 'o';
+      player2.symbolForDisplay = circle;
     } else if (selectedSymbol === 'circle') {
-      player1.symbol = circle;
-      player2.symbol = cross;
+      player1.symbol = 'o';
+      player1.symbolForDisplay = circle;
+      player2.symbol = 'x';
+      player2.symbolForDisplay = cross;
     }
 
     // hide the symbol selecting screen
@@ -43,69 +54,91 @@
     $('#cross, #circle').click(function() {
       chooseSymbol(this.id);
 
-      // if the ai can make the first move --> make first move
-      if (singlePlayer && notUsedFields.length === 9 && !isPlayer1) {
-        inputController();
+      // if the ai has the first move --> get an ai move
+      if (isSinglePlayer && actualPlayer === player2) {
+        let aiMove = newAiMove(board, actualPlayer.symbol);
+        gameController(aiMove);
       }
     });
 
     $('.game-field').click(function() {
-      inputController(this.id, parseInt($(this).attr('value'), 10));
+      gameController(this.id);
     });
   });
+
+  function gameController(field) {
+    const selectedField = parseInt(field);
+
+    if (board[selectedField] === 'e') {
+      board[selectedField] = actualPlayer.symbol;
+      displayMove(selectedField);
+    }
+  }
+
+  function displayMove(field) {
+    const selector = '#' + field;
+    $(selector).html('<i class=\"' + actualPlayer.symbolForDisplay + '\"></i>');
+  }
 }());
+
+function newAiMove(board, symbol) {
+  let state = new GameState(board, symbol);
+  let selectedField = getMove(state);
+
+  return selectedField;
+}
 
 /**
  * @param {Array} player1 - All fields of the player
  * @param {Array} player2 - All fields of the AI
  * @param {Array} notUsedFields - All fields who aren't used *
  */
-function newAiMove(player1HasFirstMove, player1, player2, notUsedFields) {
-  let board = [];
-  $('.game-field').each(function(ix, field) {
-    if ($(field).children('.fa-circle-o').length > 0) {
-      board.push('o');
-    } else if ($(field).children('.fa-times').length > 0) {
-      board.push('x');
-    }
-    else {
-      board.push('e');
-    }
-  });
-  console.log('Board read back: ' + board.toString());
+/*function newAiMove(player1HasFirstMove, player1, player2, notUsedFields) { // ToDo: move to the module above
+ let board = [];
+ $('.game-field').each(function(ix, field) {
+ if ($(field).children('.fa-circle-o').length > 0) {
+ board.push('o');
+ } else if ($(field).children('.fa-times').length > 0) {
+ board.push('x');
+ }
+ else {
+ board.push('e');
+ }
+ });
+ console.log('Board read back: ' + board.toString());
 
-  let state = new GameState(board, player2.symbol);
-  const value = getMove(state) + 1;
-  let id;
+ let state = new GameState(board, player2.symbol);
+ const value = getMove(state) + 1;
+ let id;
 
-  switch (value) {
-    case 1:
-      id = 'field-one';
-      break;
-    case 2:
-      id = 'field-two';
-      break;
-    case 3:
-      id = 'field-three';
-      break;
-    case 4:
-      id = 'field- four';
-      break;
-    case 5:
-      id = 'field-five';
-      break;
-    case 6:
-      id = 'field-six';
-      break;
-    case 7:
-      id = 'field-seven';
-      break;
-    case 8:
-      id = 'field-eight';
-      break;
-    case 9:
-      id = 'field-nine';
-      break;
-  }
-  return [id, value];
-}
+ switch (value) {
+ case 1:
+ id = 'field-one';
+ break;
+ case 2:
+ id = 'field-two';
+ break;
+ case 3:
+ id = 'field-three';
+ break;
+ case 4:
+ id = 'field- four';
+ break;
+ case 5:
+ id = 'field-five';
+ break;
+ case 6:
+ id = 'field-six';
+ break;
+ case 7:
+ id = 'field-seven';
+ break;
+ case 8:
+ id = 'field-eight';
+ break;
+ case 9:
+ id = 'field-nine';
+ break;
+ }
+ return [id, value];
+ }*/
