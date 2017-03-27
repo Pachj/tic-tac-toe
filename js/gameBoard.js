8df4f6fe608cc8abdad9a1799c7d33a8a59353f6
Pachj <Pachj@users.noreply.github.com>
2017-03-27 16:30:15 +0200

    comments added

--- js/gameBoard.js
+++ js/gameBoard.js
@@ -1,12 +1,6 @@
 /**
  * Created by Henry on 06.03.17.
  */
-
-/** ToDo: add 2 player mode
- * ToDo: change show()/hide() anchor
- * ToDo: change the h1's to p's
- */
-
 (function() {
   // the player
   const player1 = {
@@ -29,7 +23,8 @@
   let board = ['e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'];
 
   /** choose the selected symbol for player1 and the opposite for player2
-   * @param {String} selectedSymbol - the id of the selected button*/
+   * @param {String} selectedSymbol - the id of the selected button
+   */
   function chooseSymbol(selectedSymbol) { //ToDo: rename
     const cross = 'fa fa-times';
     const circle = 'fa fa-circle-o';
@@ -101,34 +96,36 @@
     $(selector).html('<i class=\"' + actualPlayer.symbolForDisplay + '\"></i>');
   }
 
+  /** ends the current game and starts a new one
+   * @param {Boolean} hasWinner - whether someone has won
+   */
   function endTheGame(hasWinner) {
+    /* Self invoking function who changes the html content in the result-screen.
+     Also displays it and hides.*/
     (function showEndScreen() {
       const endScreenSelector = $('#result-screen');
 
-      function changeTheResult() {
-        if (hasWinner) {
-          if (isSinglePlayer) {
-            if (actualPlayer === player1) {
-              endScreenSelector.children('h1').html('You are the winner!');
-            } else {
-              endScreenSelector.children('h1').html('The ai is the winner.');
-            }
+      if (hasWinner) {
+        if (isSinglePlayer) {
+          if (actualPlayer === player1) {
+            endScreenSelector.children('h1').html('You are the winner!');
           } else {
-            if (actualPlayer.symbol === 'x') {
-              endScreenSelector.children('h1').
-                  html('The player with the X has won.');
-            } else {
-              endScreenSelector.children('h1').
-                  html('The player with the O has won.');
-            }
+            endScreenSelector.children('h1').html('The ai is the winner.');
           }
         } else {
-          endScreenSelector.children('h1').
-              html('Nobody has won. Its a draw.');
+          if (actualPlayer.symbol === 'x') {
+            endScreenSelector.children('h1').
+                html('The player with the X has won.');
+          } else {
+            endScreenSelector.children('h1').
+                html('The player with the O has won.');
+          }
         }
+      } else {
+        endScreenSelector.children('h1').
+            html('Nobody has won. Its a draw.');
       }
 
-      changeTheResult();
       endScreenSelector.show('slow');
 
       function hideEndScreen() {
@@ -138,6 +135,7 @@
       setTimeout(hideEndScreen, 3000);
     })();
 
+    // self invoking function who clears the board
     (function clearTheBoard() {
       $('.game-field').each(function(index, field) {
         $(field).html('');
@@ -148,6 +146,7 @@
     player1HasFirstMove = !player1HasFirstMove;
     actualPlayer = player1HasFirstMove === true ? player1 : player2;
 
+    // gets a new ai move if the ai is the player who can make a move
     if (isSinglePlayer && actualPlayer === player2) {
       const aiMove = newAiMove(board, player2.symbol);
       gameController(aiMove);
@@ -204,58 +203,3 @@ function newAiMove(board, symbol) {
   let state = new GameState(board, symbol);
   return getMove(state);
 }
-
-/**
- * @param {Array} player1 - All fields of the player
- * @param {Array} player2 - All fields of the AI
- * @param {Array} notUsedFields - All fields who aren't used *
- */
-/*function newAiMove(player1HasFirstMove, player1, player2, notUsedFields) { // ToDo: move to the module above
- let board = [];
- $('.game-field').each(function(ix, field) {
- if ($(field).children('.fa-circle-o').length > 0) {
- board.push('o');
- } else if ($(field).children('.fa-times').length > 0) {
- board.push('x');
- }
- else {
- board.push('e');
- }
- });
- console.log('Board read back: ' + board.toString());
-
- let state = new GameState(board, player2.symbol);
- const value = getMove(state) + 1;
- let id;
-
- switch (value) {
- case 1:
- id = 'field-one';
- break;
- case 2:
- id = 'field-two';
- break;
- case 3:
- id = 'field-three';
- break;
- case 4:
- id = 'field- four';
- break;
- case 5:
- id = 'field-five';
- break;
- case 6:
- id = 'field-six';
- break;
- case 7:
- id = 'field-seven';
- break;
- case 8:
- id = 'field-eight';
- break;
- case 9:
- id = 'field-nine';
- break;
- }
- return [id, value];
- }*/
